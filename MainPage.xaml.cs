5202d8e9565d664363ed338c0e56c1f3c509214c
SulyokDavid <113507775+SulyokDavid@users.noreply.github.com>
2023-01-03 16:08:31 +0100

    Main page notes

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,61 @@
-﻿namespace ProPad;
+﻿using Microsoft.Maui.Animations;
+
+namespace ProPad;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    protected override void OnAppearing()
+    {
+        var notesList = new List<string>();
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        notesList.Add("Első bejegyzés");
+        notesList.Add("Második bejegyzés");
+        notesList.Add("Harmadik bejegyzés");
+        notesList.Add("Harmadik bejegyzés");
+        notesList.Add("Harmadik bejegyzés");
+        notesList.Add("Harmadik bejegyzés");
+        notesList.Add("Harmadik bejegyzés");
+        notesList.Add("Harmadik bejegyzés");
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        int col = 0;
+        int row = 0;
+
+        foreach (var i in notesList)
+        {
+            Frame fr = new Frame() {
+            WidthRequest = 160,
+            HeightRequest = 160,
+            Margin = 40,
+            };
+
+            Label lb = new Label() {Text = i};
+            
+            fr.Content = lb;
+
+            grid.Children.Add(fr);
+
+            grid.SetRow(fr, row);
+            grid.SetColumn(fr, col);
+
+            if (col == 0)
+            {
+                col = 1;
+            }
+            else
+            {
+                col = 0;
+                RowDefinition rd = new RowDefinition() {Height = 200};
+                grid.RowDefinitions.Add(rd);
+                row++;
+            }
+        }
+
+    }
 }
 
